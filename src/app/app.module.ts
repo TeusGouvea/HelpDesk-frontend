119b5e8bba52795c03f235d7360d5c570ec1f05a
Matheus Gouvea <matheusdossantosgouvea@gmail.com>
2025-01-09 16:12:00 -0300

    Componente LOGIN criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,6 +33,7 @@ import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
@@ -42,6 +43,7 @@ import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-
     HomeComponent,
     HeaderComponent,
     TecnicoListComponent,
+    LoginComponent,
     
   ],
   imports: [
