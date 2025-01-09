265531bcf6653dd451c598bf34f044cea94908d8
Matheus Gouvea <matheusdossantosgouvea@gmail.com>
2025-01-09 15:21:43 -0300

    Componente TECNICO-LIST criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,6 +32,7 @@ import { MatCardModule } from '@angular/material/card';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 
 
 @NgModule({
@@ -40,6 +41,7 @@ import { HeaderComponent } from './components/header/header.component';
     NavComponent,
     HomeComponent,
     HeaderComponent,
+    TecnicoListComponent,
     
   ],
   imports: [
