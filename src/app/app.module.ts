ab7dba512f566cc67b5349a4739b3b1798375bf2
Matheus Gouvea <matheusdossantosgouvea@gmail.com>
2025-01-10 17:23:47 -0300

    inteceptor para requisições Http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { ToastrModule } from 'ngx-toastr';
+import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
 
 
 @NgModule({
@@ -78,6 +79,7 @@ import { ToastrModule } from 'ngx-toastr';
     }),
 ],
  /* providers: [AuthInterceptorProvider],*/
+  providers: [AuthInterceptorProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
